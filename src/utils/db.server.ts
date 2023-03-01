827f8f68463efa4e7e3c28cdcee7e5befc24bfce
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-03-01 21:25:17 +0100

    restructuration

---
+++ src/utils/db.server.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+let prisma = new PrismaClient();
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+if (!global.prisma) global.prisma = new PrismaClient();
+
+prisma = global.prisma;
+
+export { prisma };
